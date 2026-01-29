a2f01faabc23ea0d4f095ffebe746a5a9b60bc79
JustLife <78886759+RoboBoy99@users.noreply.github.com>
2026-01-29 21:48:19 +0200

    sdad

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System.IO;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -7,22 +8,57 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float speed = 10;
     private float velX = 0;
     private float velZ = 0;
+
+
+    #region
+    public float MouseX, MouseY ;
+
+    public GameObject camera ;
+    public float xRotate = 0f, MaxTurn = 90f, MinTurn = -90f, mouseSens = 100f;
+    #endregion
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
 
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
-    // Update is called once per frame
+
+    private void OnMouse()
+    {
+
+        float x = Input.GetAxis("Mouse X") * mouseSens * Time.deltaTime;
+        float y = Input.GetAxis("Mouse Y") * mouseSens * Time.deltaTime;
+
+        xRotate -= y;
+        xRotate = Mathf.Clamp(xRotate, MinTurn, MaxTurn);
+
+        camera.transform.rotation = Quaternion.Euler(xRotate, MinTurn, MaxTurn);
+        transform.Rotate(Vector3.up * MouseX);
+
+        camera.transform.rotation = Quaternion.Euler(xRotate, MinTurn, MaxTurn);
+        transform.Rotate(Vector3.up * MouseX);
+    }
+    private void Update()
+    {
+        OnMouse();
+    }
     void FixedUpdate()
     {
+
+
         float xAxis = Input.GetAxis("Horizontal");
         float zAxis = Input.GetAxis("Vertical");
+
 
         velZ = Mathf.Clamp(zAxis * speed, -MAX_VELOCITY, MAX_VELOCITY);
         velX = Mathf.Clamp(xAxis * speed, -MAX_VELOCITY, MAX_VELOCITY);
 
 
         playerRb.linearVelocity = new Vector3(velX, 0, velZ);
+
     }
+
+
 }
