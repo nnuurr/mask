3ef17b3bc7b3bd123a7c79a91d951ca8b9077889
FortressSpy <myfictionalmail@gmail.com>
2026-01-30 21:03:52 +0200

    Weight Sensor can invoke actions + ClickableButtons prefab

--- Assets/Scripts/WeightSensor.cs
+++ Assets/Scripts/WeightSensor.cs
@@ -1,11 +1,16 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 [RequireComponent(typeof(Collider))]
 public class WeightSensor : MonoBehaviour
 {
+    [SerializeField]
+    private UnityEvent onSensorTriggered;
+
     public void ActivateSensor(Draggable src)
     {
         Debug.Log($"Weight Sensor Activated by {src.name}");
+        onSensorTriggered.Invoke();
     }    
 }
