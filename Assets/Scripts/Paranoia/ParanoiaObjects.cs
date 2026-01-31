14cf8c5f9def4535d7b3607b414d1308e2dec446
nnuurr <oferatnz@gmail.com>
2026-01-31 13:16:08 +0200

    objects

--- Assets/Scripts/Paranoia/ParanoiaObjects.cs
+++ Assets/Scripts/Paranoia/ParanoiaObjects.cs
@@ -3,16 +3,20 @@ using UnityEngine;
 public class ParanoiaObjects : MonoBehaviour
 {
     [SerializeField] private GameObject obj;
+    private BoxCollider bColl;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        bColl = GetComponent<BoxCollider>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(obj.activeSelf)
+        {
+            bColl.center = obj.transform.localPosition;
+        }
     }
     private void OnTriggerEnter(Collider other)
     {
